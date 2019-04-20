bb35d7fdb5ad85dac8532e13ab7388a832d56d5d
Huy do <huyddo40@gmail.com>
2019-04-20 03:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return 'I can\'t hear you'
 }
 
-function sayHiToGrandma(string)
+function sayHiToGrandma(string){
   if ( (string) === string.toUpperCase() ) 
     return "YES INDEED!"
 // if ( (string) === string.toUpperCase() ) return "YES INDEED!"
