72bff0b9194ece7e967656bc006d454938b20d20
Huy do <huyddo40@gmail.com>
2019-04-20 03:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function sayHiToGrandma(string){
 
 function sayHiToGrandm (string)
 if ( (string) === string.toUpperCase() ) return "YES INDEED!"
+// if ( (string) === string.toUpperCase() ) return "YES INDEED!"
 }
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma.")
